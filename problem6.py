131d2f0540cd2e097bbabd2437968448e02b69c7
JunaidKhan444 <mrjunaid444@gmail.com>
2022-02-13 10:48:36 +0530

    New problems Added

---
+++ problem6.py
@@ -0,0 +1,11 @@
+# convert all lowercase letters to uppercase letters and vice versa.
+
+
+
+def swap_case(s):        
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
